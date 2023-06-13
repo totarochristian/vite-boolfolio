ae4f632028c29fa88c2c4601bac0570aa35ec529
totarochristian <totaro.christian.18@gmail.com>
2023-06-13 15:12:28 +0200

    Add basic main component

---
+++ src/components/mainAreas/MainComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <main class="w-100 h-100 bg-custom-black text-white d-flex justify-content-center align-items-center overflow-hidden">
+    <ProjectsComponent v-if="menuElements[1].isActive"/>
+    <EmptyTitleComponent v-else />
+  </main>
+</template>
+
+<script>
+  import ProjectsComponent from '../subAreas/main/ProjectsComponent.vue';
+  import EmptyTitleComponent from '../subAreas/main/EmptyTitleComponent.vue';
+  export default {
+    name: "MainComponent",
+    components:{
+      ProjectsComponent,
+      EmptyTitleComponent
+    },
+    props:{
+      menuElements: Object
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  main{
+    min-height: 17.7vh;
+  }
+</style>
