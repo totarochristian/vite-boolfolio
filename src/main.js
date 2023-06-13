bee9ad777a0e900298e93ff8b5bfb44cfc68b5e0
totarochristian <totaro.christian.18@gmail.com>
2023-06-13 15:18:49 +0200

    Remove router referencies

--- src/main.js
+++ src/main.js
@@ -2,10 +2,5 @@ import './assets/styles/main.scss'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
 
-const app = createApp(App)
-
-app.use(router)
-
-app.mount('#app')
+const app = createApp(App).mount('#app')
