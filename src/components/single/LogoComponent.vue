08a4e44862b07adae4a292b9c60e80be46606387
totarochristian <totaro.christian.18@gmail.com>
2023-06-13 15:07:53 +0200

    Add basic logo component

---
+++ src/components/single/LogoComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <img id="logo" src="/images/dc-logo.png" alt="Logo DC">
+</template>
+
+<script>
+  export default {
+    name: "LogoComponent"
+  }
+</script>
+
+<style lang="scss" scoped>
+  #logo{
+    height: 80px;
+    width: 80px;
+  }
+  @media screen and (max-width: 1199.98px){
+    #logo{
+      height: 100px;
+      width: 100px;
+      margin: 0 auto;
+      margin-top: 2.5rem;
+    }
+  }
+</style>
