61a3a9d8a02dffc02d9001affc6d255d03389a34
totarochristian <totaro.christian.18@gmail.com>
2023-06-13 15:12:17 +0200

    Add basic projects component

---
+++ src/components/subAreas/main/ProjectsComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="projects" class="w-100 h-100 d-flex flex-column justify-content-center align-items-center">
+    Progetti
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "ProjectsComponent"
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
