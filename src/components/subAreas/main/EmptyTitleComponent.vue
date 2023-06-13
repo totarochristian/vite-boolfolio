04188d8af41715f670afb798ac63c3d0ce4debc9
totarochristian <totaro.christian.18@gmail.com>
2023-06-13 15:12:08 +0200

    Add basic empty title component

---
+++ src/components/subAreas/main/EmptyTitleComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <h2>{{ title }}</h2>
+</template>
+
+<script>
+  export default {
+    name: "EmptyTitleComponent",
+    data(){
+      return{
+        title: "--> Content goes here <--"
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
