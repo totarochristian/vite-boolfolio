635fe245b52d6345f904c5a1d7e3314efca7ed22
totarochristian <totaro.christian.18@gmail.com>
2023-06-13 17:30:47 +0200

    Add navigation bar in the projects lsit component to let the user change page of projects

--- src/components/single/ProjectsListComponent.vue
+++ src/components/single/ProjectsListComponent.vue
@@ -3,6 +3,19 @@
     <div id="projectsCards" class="px-3 py-4 d-flex justify-content-center flex-wrap gap-4">
       <ProjectCardComponent :index="index" :projectData="project" v-for="(project,index) in projects" />
     </div>
+    <nav aria-label="Page navigation">
+      <ul class="pagination">
+        <li class="page-item" :class="currentPage==1 ? 'disabled' : ''">
+          <button class="page-link" @click="getData(currentPage-1)">Precedente</button>
+        </li>
+        <li class="page-item" :class="currentPage==elem ? 'active' : ''" v-for="elem in lastPage">
+          <button class="page-link" @click="getData(elem)">{{elem}}</button>
+        </li>
+        <li class="page-item" :class="currentPage==lastPage ? 'disabled' : ''">
+          <button class="page-link" @click="getData(currentPage+1)">Successivo</button>
+        </li>
+      </ul>
+    </nav>
   </div>
 </template>
 
