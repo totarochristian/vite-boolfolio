a158f5e3d940cd6c670ae15c22052813db422aef
totarochristian <totaro.christian.18@gmail.com>
2023-06-13 15:19:17 +0200

    Add data file with menu data

---
+++ src/data/data.js
@@ -0,0 +1,44 @@
+const menuData = [
+  {
+    isActive: false,
+    text: 'characters'
+  },
+  {
+    isActive: true,
+    text: 'comics'
+  },
+  {
+    isActive: false,
+    text: 'movies'
+  },
+  {
+    isActive: false,
+    text: 'tv'
+  },
+  {
+    isActive: false,
+    text: 'games'
+  },
+  {
+    isActive: false,
+    text: 'collectibles'
+  },
+  {
+    isActive: false,
+    text: 'videos'
+  },
+  {
+    isActive: false,
+    text: 'fans'
+  },
+  {
+    isActive: false,
+    text: 'news'
+  },
+  {
+    isActive: false,
+    text: 'shop'
+  }
+];
+
+export { menuData};
