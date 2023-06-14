97004a9fc87ea48940754c604eec1cb04de8347a
totarochristian <totaro.christian.18@gmail.com>
2023-06-14 10:50:06 +0200

    Adjust the app template and home page template to put header and footer in all the pages of the site

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,24 @@
 <template>
+  <HeaderComponent :menuElements="menuElements" />
   <router-view></router-view>
+  <FooterComponent />
 </template>
 
 <script>
+  import HeaderComponent from "./components/mainAreas/HeaderComponent.vue";
+  import FooterComponent from "./components/mainAreas/FooterComponent.vue";
+  import { menuData } from './data/data';
   export default {
-    
+    name: 'App',
+    components: {
+      HeaderComponent,
+      FooterComponent
+    },
+    data(){
+      return{
+        menuElements: menuData
+      }
+    }
   }
 </script>
 
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,20 +1,16 @@
 <template>
-  <HeaderComponent :menuElements="menuElements" />
   <MainComponent :menuElements="menuElements" />
-  <FooterComponent />
 </template>
 
 <script>
-  import HeaderComponent from "../components/mainAreas/HeaderComponent.vue";
+  
   import MainComponent from "../components/mainAreas/MainComponent.vue";
-  import FooterComponent from "../components/mainAreas/FooterComponent.vue";
+  
   import { menuData } from '../data/data';
   export default {
     name: "HomePage",
     components:{
-      HeaderComponent,
-      MainComponent,
-      FooterComponent
+      MainComponent
     },
     data(){
       return{
