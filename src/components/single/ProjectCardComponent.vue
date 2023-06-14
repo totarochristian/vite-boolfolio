683b0e93ea77600a0651854a19c47eb43a8f333c
totarochristian <totaro.christian.18@gmail.com>
2023-06-14 12:46:51 +0200

    Update project card component graphic

--- src/components/single/ProjectCardComponent.vue
+++ src/components/single/ProjectCardComponent.vue
@@ -2,12 +2,9 @@
   <div :id="'project_'+index" class="project d-flex flex-column gap-2" @click="redirectToDetail">
     <div class="projectImage">
       <img :src="projectData.image" :alt="projectData.title" class="w-100 h-100">
+      <h5 class="title w-100 pt-3 fw-bold">{{ projectData.title }}</h5>
       <span class="category text-uppercase fw-bold badge bg-primary">{{ projectData.category.name }}</span>
     </div>
-    <h5 class="w-100 pt-3 fw-bold">{{ projectData.title }}</h5>
-    <p class="w-100 pt-3" v-html="projectData.body"></p>
-    <a :href="projectData.git_repository" target="_blank" class="btn btn-primary" v-if="projectData.git_repository">Github repository</a>
-    <a :href="projectData.live_site" target="_blank" class="btn btn-success" v-if="projectData.live_site">Live site</a>
   </div>
 </template>
 
@@ -35,7 +32,7 @@ import { router } from '../../router';
 </script>
 
 <style lang="scss" scoped>
-  $squareSize: 210px;
+  $squareSize: 280px;
   .project{
     width: $squareSize;
     .projectImage{
@@ -45,11 +42,19 @@ import { router } from '../../router';
       img{
         object-fit: cover;
       }
-      .category{
+      .category,.title{
         visibility: hidden;
         position: absolute;
+      }
+      .category{
         top: 10px;
         right: 10px;
+      }
+      .title{
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%,-50%);
+        text-align: center;
       }
     }
     &:hover{
@@ -58,7 +63,7 @@ import { router } from '../../router';
           img{
             filter: brightness(50%);
           }
-          .category{
+          .category,.title{
             visibility: visible;
           }
         }
