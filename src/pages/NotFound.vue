11586ac96f7f72ad38734543abea58d1fe8bc9ad
totarochristian <totaro.christian.18@gmail.com>
2023-06-14 10:33:34 +0200

    Add route to the not found page (is the last route, called when no other route matches with the path).

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Pagina non trovata, errore 404</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import {createRouter, createWebHistory} from 'vue-router';
 
 import HomePage from './pages/HomePage.vue';
 import SingleProject from './pages/SingleProject.vue';
+import NotFound from './pages/NotFound.vue';
 
 const router = createRouter({
   history: createWebHistory(),
@@ -16,6 +17,11 @@ const router = createRouter({
       name: 'project',
       component: SingleProject
     },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'not-found',
+      component: NotFound
+    }
   ]
 });
 
