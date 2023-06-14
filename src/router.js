926dbfa5e475954ad6b2bc18281146bc2c06846a
totarochristian <totaro.christian.18@gmail.com>
2023-06-14 10:22:11 +0200

    Create the router, add it the history and the basic route to the home

--- src/router.js
+++ src/router.js
@@ -2,3 +2,14 @@ import {createRouter, createWebHistory} from 'vue-router';
 
 import HomePage from './pages/HomePage';
 import SingleProject from './pages/SingleProject';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomePage
+    }
+  ]
+})
