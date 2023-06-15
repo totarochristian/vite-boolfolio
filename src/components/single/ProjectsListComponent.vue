19eddc0cf6626f78d7715404454dfdf0437d0446
totarochristian <totaro.christian.18@gmail.com>
2023-06-15 10:07:54 +0200

    Add loader component, update styles to make it function correctly and update the projects list component including the loader

--- src/components/single/ProjectsListComponent.vue
+++ src/components/single/ProjectsListComponent.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="w-75 h-100 d-flex flex-column justify-content-start align-items-center pb-4 gap-4">
+  <LoaderComponent v-if="loading"/>
+  <div class="w-75 h-100 d-flex flex-column justify-content-start align-items-center pb-4 gap-4" v-if="!loading">
     <div id="projectsCards" class="px-3 py-4 d-flex justify-content-center flex-wrap gap-4">
       <ProjectCardComponent :index="index" :projectData="project" v-for="(project,index) in projects" />
     </div>
@@ -21,18 +22,21 @@
 
 <script>
   import ProjectCardComponent from './ProjectCardComponent.vue';
+  import LoaderComponent from './LoaderComponent.vue';
   import axios from 'axios';
   export default {
     name: "ProjectsListComponent",
     components: {
-      ProjectCardComponent
+      ProjectCardComponent,
+      LoaderComponent
     },
     data(){
       return{
         projects: [],
         apiUrl: 'http://127.0.0.1:8000/api',
         currentPage: 1,
-        lastPage: 1
+        lastPage: 1,
+        loading: true
       }
     },
     methods: {
@@ -45,6 +49,8 @@
           this.projects = res.data.results.data;
           this.currentPage = res.data.results.current_page;
           this.lastPage = res.data.results.last_page;
+        }).finally(()=>{
+          this.loading = false;
         });
       }
     },
